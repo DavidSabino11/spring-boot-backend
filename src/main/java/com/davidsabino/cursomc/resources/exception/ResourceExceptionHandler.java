397b5b815d75c470c67e64fa1ad8edf1155eeb1d
David Sabino <david.sabino17@gmail.com>
2022-03-29 19:16:14 -0300

    PUT, DELETE e GET para cliente

--- src/main/java/com/davidsabino/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/davidsabino/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.davidsabino.cursomc.resources.exception;
 
-import java.lang.module.FindException;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
